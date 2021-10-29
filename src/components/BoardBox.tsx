75a78387e2dc63a8b8228c2586b5b88c602006fa
rmc3408 <rmc3408@protonmail.com>
2021-10-29 14:36:57 -0400

    part 3 done

--- src/components/BoardBox.tsx
+++ src/components/BoardBox.tsx
@@ -1,30 +1,61 @@
-import React from "react";
+import React, { ChangeEvent, Fragment, useState } from "react";
 import StudentCard from "./StudentCard";
 import { getData } from "../API/getData";
 import { useQuery } from "react-query";
-import { Row, Card, Divider } from "antd";
+import { Row, Card, Divider, Input } from "antd";
 
 const BoardBox = () => {
+  const [fullNamefiltered, setFullNamefiltered] = useState<IStudent[] | undefined>(undefined);
+
   const { isLoading, error, data } = useQuery<IStudent[], TError>(
     "students",
     () => getData()
   );
 
+  const onNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const txt = e.target.value.toLowerCase();
+    const txtFiltered = data?.filter(
+      ({ firstName, lastName }) =>
+        firstName.toLowerCase().includes(txt) ||
+        lastName.toLowerCase().includes(txt)
+    );
+    setFullNamefiltered(txtFiltered);
+  };
+
   if (isLoading) return <h1>Loading...</h1>;
   if (error) return <h1>An error has occurred</h1>;
 
+  const FullData = data?.map((stud: IStudent) => {
+    return (
+      <Fragment key={stud.id}>
+        <StudentCard {...stud} />
+        <Divider />
+      </Fragment>
+    );
+  });
+
+  const NamedFilteredData = fullNamefiltered?.map((stud: IStudent) => {
+    return (
+      <Fragment key={stud.id}>
+        <StudentCard {...stud} />
+        <Divider />
+      </Fragment>
+    );
+  });
+
   return (
     <div className="Center-BoardBox">
-      <Card style={{ overflow: 'auto' }}>
-        <Row justify="center" align="middle" >
-          {data?.map((stud: IStudent) => {
-            return (
-              <>
-                <StudentCard {...stud} />
-                <Divider />
-              </>
-            );
-          })}
+      <Input
+        size="large"
+        placeholder="Search by name"
+        onChange={onNameChange}
+        allowClear
+        className="input-Name-BoardBox"
+      />
+
+      <Card >
+        <Row justify="center" align="middle">
+          {NamedFilteredData ?? FullData}
         </Row>
       </Card>
     </div>
